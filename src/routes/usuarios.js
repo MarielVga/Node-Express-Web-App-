b1a4e2f94b779527815eb6df38c119948a1076f7
Mariela Vega <mvp.creation.design.style@gmail.com>
2026-09-09 22:12:19 -0300

    feat: implementar middleware de autenticacion JWT y proteger rutas de la API

--- src/routes/usuarios.js
+++ src/routes/usuarios.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const verificarToken = require('../middlewares/auth');
 const upload = require("../middlewares/upload");
+
 const { 
         crearUsuario, 
         getUsuarios, 
@@ -12,12 +14,17 @@ const {
     } = require('../controllers/usuarioController');
 
 // Rutas RESTful
+// Ruta publica
 router.post('/', crearUsuario);       // Crear usuario
-router.get('/', getUsuarios);         // Leer usuarios
-router.put('/:id', actualizarUsuario);    // Actualizar usuario por ID
-router.delete('/:id', eliminarUsuario);   // Eliminar usuario por ID
-router.get('/:id/pedidos', getUsuarioConPedidos); // Leer usuario especifico con sus pedidos
-router.post('/transaccion', crearUsuarioConPedido); // Crea usuarios con pedidos
-router.post('/:id/upload', upload.single('avatar'), subirAvatar); // Sube imagen del avatar
+
+// Rutas protegidas
+router.get('/', verificarToken, getUsuarios);         // Leer usuarios
+router.put('/:id', verificarToken,actualizarUsuario);    // Actualizar usuario por ID
+router.delete('/:id', verificarToken,eliminarUsuario);   // Eliminar usuario por ID
+router.get('/:id/pedidos', verificarToken,getUsuarioConPedidos); // Leer usuario especifico con sus pedidos
+router.post('/transaccion', verificarToken,crearUsuarioConPedido); // Crea usuarios con pedidos
+
+// Endpoint de subida de archivo
+router.post('/:id/upload', verificarToken, upload.single('avatar'), subirAvatar); // Sube imagen del avatar
 
 module.exports = router;
